09f12235a031b00da661b0585f7446a6ea144479
Denis Maligin <denis.maligin@chainguard.dev>
2025-07-23 15:18:59 +0200

    build-all.sh adjustments

--- build-all.sh
+++ build-all.sh
@@ -18,18 +18,28 @@ docker buildx build -t hello-go:wolfi-latest-ms -f Dockerfile.wolfi-ms .
 
 # build Wolfi using apko + melange
 ## hello-go apk
+./update-sha256-melange.sh
 melange build melange.yaml --arch host --signing-key melange.rsa
 
 ## build containers
+### dev-image
 apko build apko-dev.yaml hello-go:wolfi-latest-apko-dev hello-go-dev.tar --arch host
+### saving sboms
+mv sbom-index.spdx.json hello-go-prod-sbom-index.spdx.json
+mv sbom-x86_64.spdx.json hello-go-prod-sbom-x86_64.spdx.json
+
+### prod-image
 apko build apko-prod.yaml hello-go:wolfi-latest-apko-prod hello-go-prod.tar --arch host
+### saving sboms
+mv sbom-index.spdx.json hello-go-dev-sbom-index.spdx.json
+mv sbom-x86_64.spdx.json hello-go-dev-sbom-x86_64.spdx.json
 
 ## load containers
-docker load < hello-go:wolfi-latest-apko-dev
-docker load < hello-go:wolfi-latest-apko-prod
+docker load < hello-go-dev.tar
+docker load < hello-go-prod.tar
 
 ## remove tars
-rm -rf hello-go-dev.tar hello-go-prod.tar
+#rm -rf hello-go-dev.tar hello-go-prod.tar
 
 # list built containers
 docker image ls | grep hello-go
