92ffece4749e2c55a2bcf2bcb39483649c8d0832
Denis Maligin <denis.maligin@chainguard.dev>
2025-07-23 14:55:49 +0200

    added script for updating sha256hash in melange.yaml

---
+++ update-sha256-melange.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+YAML_FILE="melange.yaml"
+URL="https://github.com/maligin/partner-enablement-workshop/archive/refs/heads/main.tar.gz"
+
+# SHA256 automatisch ermitteln
+NEW_HASH=$(curl -sSL "$URL" | sha256sum | awk '{print $1}')
+
+# Debug-Ausgabe
+echo "Neuer SHA256: $NEW_HASH"
+
+# Wert im YAML ersetzen
+yq -i '
+  (.pipeline[] | select(.uses == "fetch") | .with.expected-sha256) = "'"$NEW_HASH"'"
+' "$YAML_FILE"
+
+echo "melange.yaml wurde aktualisiert."
+
